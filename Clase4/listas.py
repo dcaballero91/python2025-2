3f09368e4feff862146d136137319a1469022185
Derlis Caballero Mendoza <derliscaballero91@gmail.com>
2025-09-20 13:59:33 -0300

    clas4 intro

---
+++ Clase4/listas.py
@@ -0,0 +1,10 @@
+'''
+Introduccion a las listas en Python
+'''
+
+# Definicion de una lista
+array=["futbol","Pc",18.6,18,[6,7,10.5],True, False]
+print(array)
+# Acceder a un elemento de la lista
+print(array[0])
+
